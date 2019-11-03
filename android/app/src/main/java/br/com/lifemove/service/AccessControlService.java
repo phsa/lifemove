751f02ef7c9fb5c8e4e43596a5aa9d65129bf7e9
paulo.sousa <paulinho.h.s.a.spn@gmail.com>
2019-11-03 20:19:42 -0300

    refactoring/access_control: username check now asynchronous

--- android/app/src/main/java/br/com/lifemove/service/AccessControlService.java
+++ android/app/src/main/java/br/com/lifemove/service/AccessControlService.java
@@ -5,7 +5,7 @@ import java.util.Collections;
 import java.util.List;
 
 import br.com.lifemove.R;
-import br.com.lifemove.listener.SimpleAsynchronousTaskListener;
+import br.com.lifemove.listener.AccessControlListener;
 import br.com.lifemove.model.User;
 import br.com.lifemove.utils.SharedPreferencesUtils;
 import br.com.lifemove.utils.StringUtils;
@@ -14,9 +14,9 @@ public class AccessControlService {
 
     private static List<User> users = new ArrayList<>(Collections.singletonList(new User("Administrador", "admin", "admin@admin.com", "admin@admin")));
 
-    private SimpleAsynchronousTaskListener listener;
+    private AccessControlListener listener;
 
-    public AccessControlService(SimpleAsynchronousTaskListener listener) {
+    public AccessControlService(AccessControlListener listener) {
         this.listener = listener;
     }
 
@@ -45,11 +45,15 @@ public class AccessControlService {
         } else listener.onFailure(StringUtils.valueOf(R.string.authentication_has_crashed));
     }
 
-    public boolean alreadyInUse(String username) {
+    public void checkForUsername(String username) {
+        boolean isAvailable = true;
         for (User user: users)
-            if (user.getUsername().compareTo(username) == 0)
-                return true;
-        return false;
+            if (user.getUsername().compareTo(username) == 0) {
+                listener.handleUsernameCheck(username, false);
+                isAvailable = false;
+            }
+        if (isAvailable)
+            listener.handleUsernameCheck(username, true);
     }
 
 }
